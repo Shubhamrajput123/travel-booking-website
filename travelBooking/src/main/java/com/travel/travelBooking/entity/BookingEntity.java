a806afc066ac60dea020d217a093adf8d24bcfd0
shubham <singhshubham6636@gmail.com>
2025-03-02 11:35:26 +0530

    booking entity created

--- travelBooking/src/main/java/com/travel/travelBooking/entity/BookingEntity.java
+++ travelBooking/src/main/java/com/travel/travelBooking/entity/BookingEntity.java
@@ -3,6 +3,8 @@ package com.travel.travelBooking.entity;
 import org.hibernate.annotations.*;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -21,4 +23,13 @@ public class BookingEntity {
 		@JoinColumn(name="user_id", nullable=false)
 		
 		private Traveler traveler;
+		@ManyToOne
+	    @JoinColumn(name = "package_id", nullable = false)
+	    private TravelPackage travelPackage;
+		
+	    @Enumerated(EnumType.STRING)
+	    private BookingStatus status;
+
+		
+		  
 }
